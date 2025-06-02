88a58e72459df167016272b2035fdb193f6bc0b4
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-06-02 18:03:56 +0530

    Login Register View remaining

---
+++ ServiceMesh.Web/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceMesh.Services.Web.Models.DTO;
+using ServiceMesh.Web.Service.IService;
+
+namespace ServiceMesh.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService authService;
+
+        public AuthController(IAuthService _authService)
+        {
+            authService = _authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
---
+++ ServiceMesh.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using ServiceMesh.Services.Web.Models.DTO;
+using ServiceMesh.Web.Models;
+
+namespace ServiceMesh.Web.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
+        Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto);
+        Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto);
+    }
+}
