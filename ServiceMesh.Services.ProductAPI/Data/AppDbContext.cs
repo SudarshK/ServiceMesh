94ea9e457082e6f97b31841235ae319261740556
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-07-08 17:35:48 +0530

    ProductAPI

---
+++ ServiceMesh.Services.ProductAPI/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ServiceMesh.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ServiceMesh.Services.ProductAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // Required for Identity
+
+            
+
+        }
+    }
+}
---
+++ ServiceMesh.Services.ProductAPI/Models/DTO/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServiceMesh.Services.ProductAPI.Models.DTO
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
