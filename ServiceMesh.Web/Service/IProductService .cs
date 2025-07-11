cec093807f853056464786f6d3dd4e670512f99b
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-07-11 16:27:15 +0530

    74.Assignment

---
+++ ServiceMesh.Web/Service/IProductService .cs
@@ -0,0 +1,15 @@
+﻿using ServiceMesh.Services.Web.Models.DTO;
+using ServiceMesh.Web.Models;
+
+namespace ServiceMesh.Web.Service
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetProductAsync(string productCode);
+        Task<ResponseDto?> GetAllProductAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> CreateProductAsync(ProductDto productCode);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productCode);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
