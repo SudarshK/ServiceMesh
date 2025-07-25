ccd644684ae58b7b76e5102460a0ef955bc8bd9e
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-07-25 16:37:15 +0530

    Added Coupon API in Shopping CArt

--- ServiceMesh.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ ServiceMesh.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -19,13 +19,15 @@ namespace ServiceMesh.Services.ShoppingCartAPI.Controllers
         private ResponseDto _response;
         private IMapper _mapper;
         private readonly IProductService _productService;
+        private readonly ICouponService _couponService;
 
-        public CartAPIController(AppDbContext db, IMapper mapper,IProductService productService)
+        public CartAPIController(AppDbContext db, IMapper mapper,IProductService productService, ICouponService couponService)
         {
             _db = db;
             _response = new ResponseDto();
             _mapper = mapper;
             _productService = productService;
+            _couponService = couponService;
         }
 
         [HttpGet("GetCart/{userId}")]
@@ -46,6 +48,17 @@ namespace ServiceMesh.Services.ShoppingCartAPI.Controllers
                     item.Product = productDtos.FirstOrDefault(u=> u.ProductId == item.ProductId);
                     cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
                 }
+
+                //apply Coupon logic
+                if(!string.IsNullOrEmpty(cart.CartHeader.CouponCode))
+                {
+                    CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+                    if (coupon != null && cart.CartHeader.CartTotal>coupon.MinimumAmount)
+                    {
+                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cart.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                }
                 _response.Result = cart;
             }
             catch (Exception ex)
@@ -55,6 +68,45 @@ namespace ServiceMesh.Services.ShoppingCartAPI.Controllers
             }
             return _response;
         }
+
+        [HttpPost("ApplyCoupon")]
+        public async Task<object> ApplyCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb= await _db.CartHeaders.FirstAsync(u=> u.UserId==cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = cartDto.CartHeader.CouponCode;
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result= true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCoupon")]
+        public async Task<object> RemoveCoupon([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                var cartFromDb= await _db.CartHeaders.FirstAsync(u=> u.UserId==cartDto.CartHeader.UserId);
+                cartFromDb.CouponCode = "";
+                _db.CartHeaders.Update(cartFromDb);
+                await _db.SaveChangesAsync();
+                _response.Result= true;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.ToString();
+            }
+            return _response;
+        }
+
 
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
---
+++ ServiceMesh.Services.ShoppingCartAPI/Service/IServices/ICouponService.cs
@@ -0,0 +1,10 @@
+﻿using ServiceMesh.Services.ShoppingCartAPI.Models;
+using ServiceMesh.Services.ShoppingCartAPI.Models.DTO;
+
+namespace ServiceMesh.Services.ShoppingCartAPI.Service.IServices
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetCoupon(string couponCode);
+    }
+}
