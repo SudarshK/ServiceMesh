659b7147c68a290e7a1550e1a73b1ad729871105
ArbinIndia-Sudarshan <161278920+ArbinIndia-Sudarshan@users.noreply.github.com>
2025-11-28 17:21:06 +0530

    rewards API configuration

---
+++ ServiceMesh.Services.RewardAPI/Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceMesh.Services.RewardAPI.Data;
+using ServiceMesh.Services.RewardAPI.Dto;
+using ServiceMesh.Services.RewardAPI.Models;
+using System.Text;
+
+namespace ServiceMesh.Services.EmailAPI.Services
+{
+    public class RewardService : IRewardService
+    {
+        private DbContextOptions<AppDbContext> _dbOptions;
+
+        public RewardService(DbContextOptions<AppDbContext> dbOptions)
+        {
+            _dbOptions = dbOptions;
+        }
+
+        public async Task UpdateRewards(RewardMessage rewardMessage)
+        {
+            try
+            {
+                Rewards rewards = new()
+                {
+                    OrderId = rewardMessage.OrderId,
+                    RewardsActivity = rewardMessage.RewardsActivity,
+                    UserId = rewardMessage.UserId,
+                    RewardsDate = DateTime.Now,
+                };
+                await using var db = new AppDbContext(_dbOptions);
+                await db.Rewards.AddAsync(rewards);
+                await db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                // log exception
+            }
+        }
+    }
+}
---
+++ ServiceMesh.Services.RewardAPI/Services/IRewardService.cs
@@ -0,0 +1,10 @@
+﻿using ServiceMesh.Services.RewardAPI;
+using ServiceMesh.Services.RewardAPI.Dto;
+
+namespace ServiceMesh.Services.EmailAPI.Services
+{
+    public interface IRewardService
+    {
+        Task UpdateRewards(RewardMessage rewardMessage);
+    }
+}
